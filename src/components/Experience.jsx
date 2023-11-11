f99d03e363a551bc6529240bec196161db23442e
Diyansh Rao <87599592+DiyanshRao@users.noreply.github.com>
2023-11-11 15:58:09 +0530

    chamges

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -15,7 +15,7 @@ const ExperienceCard = ({ experience }) => {
       <div className=''>
         <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
         <p
-          className='text-secondary text-[16px] font-semibold'
+          className='text-secondary text-[16px] font-semibold text-white'
           style={{ margin: 0 }}
         >
           {experience.company_name}
@@ -26,7 +26,7 @@ const ExperienceCard = ({ experience }) => {
         {experience.points.map((point, index) => (
           <li
             key={`experience-point-${index}`}
-            className='text-white-100 text-[14px] pl-1 tracking-wider'
+            className='text-white-100 text-[14px] pl-1 tracking-wider text-white'
           >
             {point}
           </li>
@@ -41,7 +41,7 @@ const Experience = () => {
     <div>
       <motion.div variants={textVariant()}>
         <p
-          className={`sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider text-center mt-10`}
+          className={`sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider text-center mt-10 text-white`}
         >
           What I have done so far
         </p>
