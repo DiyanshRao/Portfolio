5e518de4a82f08030e81344da1f93e21f51d416f
Diyansh Rao <87599592+DiyanshRao@users.noreply.github.com>
2023-11-11 15:55:22 +0530

    chamges

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -74,14 +74,14 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? 'hidden' : 'flex'
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl text-white`}
           >
-            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4 bg-black rounded-sm px-2 py-5'>
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4 bg-black rounded-sm px-2 py-5 text-white'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
                   className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                    active === nav.title ? 'text-white' : 'text-secondary'
+                    active === nav.title ? 'text-white' : 'text-gray-400'
                   }`}
                   onClick={() => {
                     setToggle(!toggle)
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -50,14 +50,16 @@ const ProjectCard = ({
 
         <div className='mt-5 text-left'>
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px] '>{description}</p>
+          <p className='mt-2 text-secondary text-[14px] text-white'>
+            {description}
+          </p>
         </div>
 
         <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
             <p
               key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
+              className={`text-[14px] text-white ${tag.color}`}
             >
               #{tag.name}
             </p>
